0fea736c4846397805855bcc8dd2a375bdf9e677
Jiaxin Li <jiaxin.1995@icloud.com>
2019-10-29 12:40:00 -0700

    fixes

--- ceph-node-setup-account.sh
+++ ceph-node-setup-account.sh
@@ -1,7 +1,5 @@
 
-# add user and config access
-if [[ ! `id -u ceph 2>/dev/null` ]]; then sudo useradd -d /home/ceph -s /bin/bash -m ceph; fi
-sudo usermod --password $(echo ceph | openssl passwd -1 -stdin) ceph
-sudo usermod -aG sudo ceph
-echo 'ceph ALL = (root) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ceph
-sudo chmod 0440 /etc/sudoers.d/ceph
+# config password-less access
+USER=`whoami`
+echo "$USER ALL = (root) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/$USER"
+sudo chmod 0440 "/etc/sudoers.d/$USER"
--- ceph-node-setup.sh
+++ ceph-node-setup.sh
@@ -3,22 +3,19 @@
 # usage
 if [[ $# -lt 1 ]] ; then echo -e "Usage: $0 <user@hosts>...\nInitlalizes the ceph account on target hosts, requires an existing sudo user"; exit -1; fi
 
-# must execute as the ceph user
-if [[ `whoami` != "ceph" ]] ; then echo "Must execute as the ceph user!"; exit -1; fi
+# must NOT execute as the ceph user
+if [[ `whoami` == "ceph" ]] ; then echo "Cannot execute as the ceph user!"; exit -1; fi
 
 # create ssh key pairs if necessary
 if [[ ! -f ~/.ssh/id_rsa ]]; then ssh-keygen; fi
 
 for i in ${@}; do
 
-    USERNAME=`echo $i | cut -d '@' -f 1`;
-    HOST=`echo $i | cut -d '@' -f 2`;
-    echo $USERNAME@$HOST
+    # setup password-less ssh
+    ssh-copy-id $i
+    ssh -t $i -- bash < ceph-node-setup-account.sh;
 
-    # create ceph user and add priviledges
-    ssh -t $USERNAME@$HOST -- bash < ceph-node-setup-account.sh;
-    ssh-copy-id ceph@$HOST;
+    # execute setup script
+    ssh -t $i -- bash < ceph-node-setup-local.sh;
 
-    # execute setup script as ceph user
-    ssh -t ceph@$HOST -- bash < ceph-node-setup-local.sh;
 done
